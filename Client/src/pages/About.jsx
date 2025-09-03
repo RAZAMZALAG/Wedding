33d07159c496ce4d5afc258d3b10c0817ed33949
Raz Amzaleg <raz.amzaleg@campus.technion.ac.il>
2025-09-03 20:05:57 +0300

    remove outside link from the about page

--- Client/src/pages/About.jsx
+++ Client/src/pages/About.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import placeHolderImage from "../assets/tool_placeholder.svg";
-// Note: WhatsApp icon removed - using placeholder instead
+// Note: All placeholder images removed as they were not working
 import {
   Box,
   Typography,
   Container,
-  Link,
   ListItem,
   List,
 } from "@mui/material";
@@ -33,7 +31,7 @@ function About() {
             mb: 4,
           }}
         >
-          <Box sx={{ flex: 1 }}>
+          <Box>
             <Typography sx={{ fontSize: "18px", lineHeight: "1.6", mb: 2 }}>
               {t("about_paragraph_1")}
             </Typography>
@@ -43,25 +41,11 @@ function About() {
             <Typography sx={{ fontSize: "18px", lineHeight: "1.6", mb: 2 }}>
               {t("about_paragraph_3")}
             </Typography>
-          </Box>
-          <Box sx={{ flex: 1 }}>
-            <img
-              src={placeHolderImage}
-              alt="About Us"
-              style={{
-                width: "100%",
-                maxWidth: 300,
-                height: "auto",
-                borderRadius: "8px",
-                display: "block",
-                margin: "0 auto",
-              }}
-            />
             <Typography
               sx={{
                 fontSize: "18px",
                 lineHeight: "1.6",
-                mt: { xs: 2, md: 4 },
+                mt: 2,
               }}
             >
               {t("about_paragraph_4")}
@@ -77,15 +61,10 @@ function About() {
             p: { xs: 2, sm: 3 },
             borderRadius: "8px",
             boxShadow: 2,
-            display: "flex",
-            flexDirection: { xs: "column", md: "row-reverse" },
-            alignItems: "center",
-            justifyContent: "space-between",
-            gap: { xs: 2, md: 4 },
             mb: 4,
           }}
         >
-          <Box sx={{ flex: 1 }}>
+          <Box>
             <Typography
               sx={{
                 fontSize: "18px",
@@ -97,49 +76,22 @@ function About() {
             </Typography>
             <List sx={{ listStyleType: "disc" }}>
               <ListItem sx={{ display: "list-item" }}>
-                <Link
-                  color={"inherit"}
-                  href="https://www.facebook.com/EliyahuToolibrary/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                <Typography color="inherit">
                   {t("about_link_1")}
-                </Link>
+                </Typography>
               </ListItem>
               <ListItem sx={{ display: "list-item" }}>
-                <Link
-                  color={"inherit"}
-                  href="https://www.facebook.com/hakolboynik/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                <Typography color="inherit">
                   {t("about_link_2")}
-                </Link>
+                </Typography>
               </ListItem>
               <ListItem sx={{ display: "list-item" }}>
-                <Link
-                  color={"inherit"}
-                  href="https://www.chicagotoollibrary.org/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                <Typography color="inherit">
                   {t("about_link_3")}
-                </Link>
+                </Typography>
               </ListItem>
             </List>
           </Box>
-          <img
-            src={placeHolderImage}
-            alt="Services"
-            style={{
-              width: "100%",
-              maxWidth: 250,
-              height: "auto",
-              borderRadius: "8px",
-              display: "block",
-              margin: "0 auto",
-            }}
-          />
         </Box>
 
         {/* Third Box - Image Left */}
@@ -150,28 +102,12 @@ function About() {
             p: { xs: 2, sm: 3 },
             borderRadius: "8px",
             boxShadow: 2,
-            display: "flex",
-            flexDirection: { xs: "column", md: "row" },
-            alignItems: "center",
-            gap: { xs: 2, md: 4 },
             mb: 4,
           }}
         >
-          <Typography sx={{ fontSize: "18px", lineHeight: "1.6", flex: 1 }}>
+          <Typography sx={{ fontSize: "18px", lineHeight: "1.6" }}>
             {t("about_paragraph_6")}
           </Typography>
-          <img
-            src={placeHolderImage}
-            alt="Location"
-            style={{
-              width: "100%",
-              maxWidth: 300,
-              height: "auto",
-              borderRadius: "8px",
-              display: "block",
-              margin: "0 auto",
-            }}
-          />
         </Box>
 
         {/* Footer */}
@@ -230,104 +166,30 @@ function About() {
           >
             {/* WhatsApp Row */}
             <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
-              <a
-                href="https://api.whatsapp.com/send?phone=%E2%81%A6+972532483676&text=%D7%94%D7%99%D7%99%20%D7%A9%D7%95%D7%9C%D7%94!%20%D7%A8%D7%A6%D7%99%D7%AA%D7%99%20%D7%9C%D7%A9%D7%90%D7%95%D7%9C"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  textDecoration: "none",
-                }}
-              >
-                <img
-                  src={placeHolderImage}
-                  alt="WhatsApp Logo"
-                  style={{
-                    height: "40px",
-                    width: "40px",
-                  }}
-                />
-              </a>
-              <a
-                href="https://api.whatsapp.com/send?phone=%E2%81%A6+972532483676&text=%D7%94%D7%99%D7%99%20%D7%A9%D7%95%D7%9C%D7%94!%20%D7%A8%D7%A6%D7%99%D7%AA%D7%99%20%D7%9C%D7%A9%D7%90%D7%95%D7%9C"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{
+              <Typography
+                sx={{
                   fontSize: "18px",
                   color: "inherit",
-                  textDecoration: "none",
                 }}
               >
                 {t("about_contact_whatsapp")}
-              </a>
+              </Typography>
             </Box>
 
             {/* Facebook Row */}
             <Box sx={{ display: "flex", alignItems: "center", gap: "16px" }}>
-              <a
-                href="https://www.facebook.com/shula.tveria15"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  textDecoration: "none",
-                }}
-              >
-                <img
-                  src={placeHolderImage}
-                  alt="Facebook Logo"
-                  style={{
-                    height: "40px",
-                    width: "40px",
-                  }}
-                />
-              </a>
-              <a
-                href="https://www.facebook.com/shula.tveria15"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{
+              <Typography
+                sx={{
                   fontSize: "18px",
                   color: "inherit",
-                  textDecoration: "none",
                 }}
               >
                 {t("about_contact_facebook")}
-              </a>
+              </Typography>
             </Box>
           </Box>
 
-          {/* Left Section with Logos */}
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              gap: "16px",
-              justifyContent: { xs: "center", md: "flex-end" },
-              mt: { xs: 2, md: 0 },
-            }}
-          >
-            <img
-              src={placeHolderImage}
-              alt="Tiberias Logo"
-              style={{
-                height: "70px",
-                width: "70px",
-                borderRadius: "8px",
-              }}
-            />
-            <img
-              src={placeHolderImage}
-              alt="Hadar Logo"
-              style={{
-                height: "70px",
-                width: "70px",
-                borderRadius: "8px",
-              }}
-            />
-          </Box>
+          {/* Left Section - removed logos */}
         </Box>
       </Container>
     </Box>
